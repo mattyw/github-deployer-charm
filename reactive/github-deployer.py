3741a070b547478266ca06a9dcc14aa6ae735f5f
mattyw <gh@mattyw.net>
2015-10-27 17:56:57 +0000

    All: init

---
+++ reactive/github-deployer.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+from charmhelpers.core import hookenv
+from charms.reactive import hook
+from charmhelpers.fetch import install_remote
+
+
+@hook('config-changed')
+def config_changed():
+    config = hookenv.config()
+    if config.changed('repo'):
+        repo = config['repo']
+        if repo != "":
+            hookenv.status_set('waiting', 'cloning %s' % repo)
+            dest = install_remote(repo)
+            hookenv.status_set('waiting', 'cloned %s' % repo)
+            # Consider payloads
+            subprocess.check_call(
+                os.path.join(dest, "github-deployer"),
+                cwd=dest,
+                )
+            hookenv.status_set('active', 'Ready')
